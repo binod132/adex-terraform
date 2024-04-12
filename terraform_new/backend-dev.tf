a03e9abd702d61dcc62d22e65e7d54a55a253cd2
Binod Adhikari <074msice006.binod@pcampus.edu.np>
2024-04-12 11:55:27 +0545

    workflows file

--- terraform_new/backend-dev.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraformstate132"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    #dynamodb_table = "MY_DB"
-  }
-}
--- terraform_new/backend-prod.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "backendtf-prod"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    #dynamodb_table = "MY_DB"
-  }
-}
